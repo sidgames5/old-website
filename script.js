878b3a1a7a4d430f1c36e75699d4d949e4ed627d
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-22 11:42:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function gotoGithubAccount() {
   window.location.href = "https://github.com/basiccorruption/";
 }
-
 function gotoYoutubeChannel() {
   window.location.href = "https://youtube.com/sidgames5";
 }
-
 function gotoTiktokChannel() {
   window.location.href = "https://tiktok.com/@sidgames5";
 }
-
 function gotoTwitterPage() {
   window.location.href = "https://twitter.com/sidgames101";
+}
+function gotoGamesPage(from) {
+  switch (from) {
+    case "home":
+      window.location.href = "games/";
+      break;
+  }
 }
