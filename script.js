5259fe19ef85c6d9b7cd9b2083c486947398a742
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-22 10:12:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function gotoGithubAccount() {
 function gotoYoutubeChannel() {
   window.location.href = "https://youtube.com/sidgames5";
 }
+
+function gotoTiktokChannel() {
+  window.location.href = "https://tiktok.com/@sidgames5";
+}
+
+function gotoTwitterPage() {
+  window.location.href = "https://twitter.com/sidgames101";
+}
