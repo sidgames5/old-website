80e8b5b892c3a27a79f77074940fa91f6ed5a7bc
Sid <saytleader@outlook.com>
2022-05-06 10:54:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,15 @@ function gotoProject(name) {
 function forceHome() {
   window.location.href = "https://basiccorruption.github.io/";
 }
+
+var i = 0;
+var txt = 'This is the typewriter effect. I totally didn\'t steal this code from w3schools.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
