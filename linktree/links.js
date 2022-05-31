998baed7811923cf1a4e315f4d663916c07b4d32
BasicCorruption <saytleader@outlook.com>
2022-05-30 20:14:01 -0400

    Create links.js

---
+++ linktree/links.js
@@ -0,0 +1,27 @@
+function goto(place) {
+    switch (place) {
+        case "youtube":
+            window.location.href = "https://youtube.com/sidgames5";
+            break;
+        case "twitch":
+            window.location.href = "https://twitch.tv/sidplayzlive";
+            break;
+        case "twitter":
+            window.location.href = "https://twitter.com/sidgames101";
+            break;
+        case "discord":
+            window.location.href = "https://discord.gg/peNdGRTfgj";
+            break;
+        case "tiktok":
+            window.location.href = "https://www.tiktok.com/@sidgames5";
+            break;
+        case "github":
+            window.location.href = "https://github.com/BasicCorruption";
+            break;
+        case "homepage":
+            window.location.href = "https://basiccorruption.github.io/";
+
+        default:
+            break;
+    }
+}
