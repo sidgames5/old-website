a222d7db8f3b03d316f1db4d933135b6018750f7
BasicCorruption <saytleader@outlook.com>
2023-07-05 12:50:32 -0400

    Completely update website

--- linktree/links.js
+++ linktree/links.js
@@ -1,27 +1,41 @@
 function goto(place) {
-    switch (place) {
-        case "youtube":
-            window.location.href = "https://youtube.com/sidgames5";
-            break;
-        case "twitch":
-            window.location.href = "https://twitch.tv/sidgames5";
-            break;
-        case "twitter":
-            window.location.href = "https://twitter.com/sidgames101";
-            break;
-        case "discord":
-            window.location.href = "https://discord.gg/peNdGRTfgj";
-            break;
-        case "tiktok":
-            window.location.href = "https://www.tiktok.com/@sidgames5";
-            break;
-        case "github":
-            window.location.href = "https://github.com/BasicCorruption";
-            break;
-        case "homepage":
-            window.location.href = "https://basiccorruption.github.io/";
+	switch (place) {
+		case "ch.youtube1":
+			window.location.href = "https://youtube.com/@sidgames5";
+			break;
+		case "ch.youtube2":
+			window.location.href = "https://youtube.com/@sidgames6";
+			break;
+		case "se.computer":
+			window.location.href =
+				"https://sidgames5.github.io/setup/computer.html";
+			break;
+		case "se.audio":
+			window.location.href =
+				"https://sidgames5.github.io/setup/audio.html";
+			break;
+		case "se.setup":
+			window.location.href =
+				"https://sidgames5.github.io/setup/index.html";
+			break;
+		case "se.servers":
+			window.location.href =
+				"https://sidgames5.github.io/setup/servers.html";
+			break;
+		case "ms.github":
+			window.location.href = "https://github.com/sidgames5";
+			break;
+		case "ms.homepage":
+			window.location.href = "https://sidgames5.github.io/";
+			break;
+		case "ms.archsmp":
+			window.location.href = "http://archsmp.ddns.net";
+			break;
+		case "ms.itchio":
+			window.location.href = "https://basiccorruption.itch.io";
+			break;
 
-        default:
-            break;
-    }
+		default:
+			break;
+	}
 }
--- script.js
+++ script.js
@@ -1,71 +1,66 @@
 function gotoGithubAccount() {
-  window.location.href = "https://github.com/basiccorruption/";
+	window.location.href = "https://github.com/sidgames5/";
 }
 function gotoYoutubeChannel() {
-  window.location.href = "https://youtube.com/sidgames5";
-}
-function gotoTiktokChannel() {
-  window.location.href = "https://tiktok.com/@sidgames5";
+	window.location.href = "https://youtube.com/@sidgames5";
 }
 function gotoTwitterPage() {
-  window.location.href = "https://twitter.com/sidgames101";
+	window.location.href = "https://twitter.com/sidgames101";
 }
 function gotoGamesPage(from) {
-  switch (from) {
-    case "home":
-      window.location.href = "games/";
-      break;
-  }
+	switch (from) {
+		case "home":
+			window.location.href = "games/";
+			break;
+	}
 }
 function goHome(num) {
-  switch (num) {
-    case 1:
-      window.location.href = "../";
-      break;
-  }
+	switch (num) {
+		case 1:
+			window.location.href = "../";
+			break;
+	}
 }
 function gotoDiscordServer() {
-  window.location.href = "https://discord.com/invite";
+	window.location.href = "https://discord.com/invite";
 }
 function gotoSoundSpace() {
-  window.location.href = "https://keystone-games.github.io/soundspace/";
+	window.location.href = "https://keystone-games.github.io/soundspace/";
 }
 function gotoProjectsPage(from) {
-  switch (from) {
-    case "home":
-      window.location.href = "projects/";
-      break;
+	switch (from) {
+		case "home":
+			window.location.href = "projects/";
+			break;
 
-    default:
-      break;
-  }
+		default:
+			break;
+	}
 }
 function gotoProject(name) {
-  switch (name) {
-    case "codeMatrix":
-      window.location.href = "../codematrix/";
-      break;
-  }
+	switch (name) {
+		case "codeMatrix":
+			window.location.href = "../codematrix/";
+			break;
+	}
 }
 function forceHome() {
-  window.location.href = "https://basiccorruption.github.io/";
+	window.location.href = "https://sidgames5.github.io/";
 }
 
 var i = 0;
-var txt = 'This is the typewriter effect. I totally didn\'t steal this code from w3schools.'; /* The text */
+var txt =
+	"This is the typewriter effect. I totally didn't steal this code from w3schools."; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typewriter").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+	if (i < txt.length) {
+		document.getElementById("typewriter").innerHTML += txt.charAt(i);
+		i++;
+		setTimeout(typeWriter, speed);
+	}
 }
 typeWriter();
-
-
-
 
 // var fixedTop = $(".fixed").offset().top;
 
